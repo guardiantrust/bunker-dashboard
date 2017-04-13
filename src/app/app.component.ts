65bf387abd11ac7256ab126cd9261e969826ea4f
guardiantrust <aaronpeterson3@gmail.com>
2017-04-13 16:16:38 -0600

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,32 +10,9 @@ export class AppComponent {
   motto = 'Plug-in. Plan. Produce.';
 }
 
-export class Company {
-  ID: string;
-  Name: string;
-  Address1:string;
-  Address2:string;
-  City: string;
-  Postal: string;
-  PhoneNumber: string;
-  ContactEmail: string;
-  IsSuspended: boolean;
-  IsActive: boolean;
-}
 
-export class User {
-  ID: string;
-  FirstName: string;
-  LastName: string;
-  Email: string;
-  PhoneNumber: string;
-  Username: string;
-  Userlevel: number;
-  IsActive: boolean;
-  SMS: string;
-  Created: string;
-  CompanyID: string;
-}
+
+
 
 
 
